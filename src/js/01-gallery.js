d6007067cd281f8529c42140b7d2bae9ce12a30a
Volodimyr213 <Terminal213@gmail.com>
2024-02-27 11:27:26 +0200

    one

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this linen
+
+const ulHtml = document.querySelector(".gallery")
+
+ function createMarcaupFn (items) {
+     return items.map(({description,original,preview})=> 
+     `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+       />
+     </a>
+   </li>`
+     ).join("")
+ }  
+ ulHtml.insertAdjacentHTML("beforeend",createMarcaupFn(galleryItems));
+
+ const lightbox =  new SimpleLightbox('.gallery a', { 
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,});
+
+ 
